68c2348da38f955470b85acf9e739c44c6e71dcb
Your Name <you@example.com>
2025-10-29 12:33:41 +0100

    milestone 2: complete filter with useMemo instead of useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useMemo, useState } from 'react'
 
 import './index.css'
 
 function App() {
 
   const [politicians, setPoliticians] = useState([])
+  const [searchPolitician, setSearchPolitician] = useState('')
 
   useEffect(() => {
 
@@ -12,21 +13,37 @@ function App() {
       .then(res => res.json())
       .then(post => {
         setPoliticians(post)
-        console.log('Politicians post', post)
+
       })
 
   }, [])
+
+
+  const filteredPoliticians = useMemo(() => {
+    return politicians.filter(politician => {
+      const nameMatch = politician.name.toLowerCase().includes(searchPolitician.toLowerCase())
+      const biographyMatch = politician.biography.toLowerCase().includes(searchPolitician.toLowerCase())
+      return nameMatch || biographyMatch
+
+    })
+  }, [politicians, searchPolitician])
+
 
 
 
 
   return (
     <>
-      <div className="container my-5">
+      <div className="container my-5 m-0-auto">
         <h1 className="text-center mb-4">Lista Politici</h1>
-
+        <div className="input d-flex justify-content-center">
+          <input type="text"
+            className='my-2'
+            value={searchPolitician}
+            onChange={(e) => setSearchPolitician(e.target.value)} />
+        </div>
         <div className="row g-4">
-          {politicians.map(politician => (
+          {filteredPoliticians.map(politician => (
             <div key={politician.id} className="col-md-6 col-lg-4">
               <div className="card h-100">
                 <img
